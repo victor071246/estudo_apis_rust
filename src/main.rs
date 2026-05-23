616ea515a95c5120dd8ddb0b57b4aae1d8ea3c16
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2026-05-23 03:00:52 -0300

    começando o projeto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<()> {
+    let conn = Connection::open("c2.db")?;
+
+    conn.execute_batch("
+        CREATE TABLE IF NOT EXISTS sessions (
+            id INTEGER PRIMARY KEY,
+            agent_id TEXT NOT NULL,
+            last_seen TEXT NOT NULL
+            )
+        ")?;
+
+    Ok(())
 }
